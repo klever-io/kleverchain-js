500b8b5a94aba47cbd5649864e6e6dd7acafcda1
Wil Macedo <wil.macedo.sa@gmail.com>
2022-02-17 14:34:04 -0300

    setup project

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+declare const test: () => string;
+export default test;
---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test = () => {
+    return "Hello World";
+};
+exports.default = test;
