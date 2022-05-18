800ba30462c84d68ed4c983532b4f6891c236ae7
Yuri Lozorio <yurilozorio@gmail.com>
2022-05-17 21:36:02 -0300

    new build function

--- build.js
+++ build.js
@@ -1,24 +1,29 @@
 #!/usr/bin/env node
 
+const path = require("path");
 const https = require("https");
 const fs = require("fs");
 
-((url, dest, cb) => {
-  console.log(__dirname);
-  const file = fs.createWriteStream(dest);
+(() => {
+  const bin = "KleverSDK.wasm";
+  const url = `https://kleverchain-wasm.s3.eu-central-1.amazonaws.com/${bin}`;
+  const dest = path.join(__dirname, "..", "..", "..", "public");
+
+  try {
+    fs.mkdirSync(dest, { recursive: true });
+  } catch (e) {
+    if (e.code != "EEXIST") throw e;
+  }
+
+  const file = fs.createWriteStream(path.join(dest, bin), {});
   https
     .get(url, function (response) {
       response.pipe(file);
       file.on("finish", function () {
-        file.close(cb);
+        file.close();
       });
     })
     .on("error", function (err) {
-      fs.unlink(dest);
-      if (cb) cb(err.message);
+      fs.unlink(path.join(dest, bin));
     });
-})(
-  "https://kleverchain-wasm.s3.eu-central-1.amazonaws.com/KleverSDK.wasm",
-  "../../public",
-  () => {}
-);
+})();
